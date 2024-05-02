81a4aeef18c114e91b47d8f50ac20c67fc3fc284
Ignacio Coria de Bernardi <ignaciocoriadb@gmail.com>
2024-05-02 21:28:04 +0000

    allClub

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -11,76 +11,10 @@ sap.ui.define([
 
         return BaseController.extend("com.sofftek.aca20241q.controller.Main", {
             onInit: function () {
-                that = this;
-            
+                that = this;   
             },
 
-
-            /*************************CRUD CLUB**************************************** */
-            onOpenDialog: function() {
-                if (!this.fDialog) {
-                    this.loadFragment({
-                        name: "com.sofftek.aca20241q.view.fragments.DialogForm",
-                        Controller: that
-                    }).then(function(oDialog) {
-                        that.getView().addDependent(oDialog);
-                        let oModel = new sap.ui.model.json.JSONModel({
-                            "IdClub":"",
-                            "Name": "",
-                            "City": "",
-                            "Country": "",
-                            "League": "",
-                            "FoundationDate": new Date()
-                        });
-                        oModel.setDefaultBindingMode("TwoWay");
-                        oDialog.setModel(oModel, "ClubCreate");
-                        oDialog.attachAfterClose(that._afterCloseDialog);
-                        this.fDialog = oDialog;
-                        this.fDialog.open();
-                    }.bind(this));
-                } else {
-                    this.fDialog.open();
-                }
-            },
-            
-            onCreateClub:function(oEvent){
-              
-                var oModel = oEvent.getSource().getModel("ClubCreate");
-                var sFoundationDate = oModel.getProperty("/FoundationDate");
-              
-
-                // Formatear la fecha en el formato (yyyy-MM-dd'T'00:00:00)
-                var oDate = new Date(sFoundationDate);
-                var sFormattedDate = oDate.toISOString().split('T')[0] + 'T00:00:00';
-            
-                // Actualizar el valor de la fecha en el modelo
-                oModel.setProperty("/FoundationDate", sFormattedDate);
-               
-                
-                // Ahora puedes acceder a los datos modificados en el modelo y enviarlos al backend
-                let oData = oModel.getData();
-                console.log(oData);
-                let oDataModel = that.getView().getModel();
-                oDataModel.create("/ClubSet", oData, {
-                    success: function (oResponse) {
-                        let result = oResponse?.results;
-                        sap.m.MessageBox.success("Club Creado");
-                        that.getOwnerComponent().getModel().refresh(true, true);
-                        that.onCloseClubCreate();
-                    },
-                    error: function (oError) {
-                        // manejar excepción del servicio
-                        sap.m.MessageBox.error("Ocurrió un problema al crear el club");
-                    }
-                });
-        
-            },
-            onCloseClubCreate: function (oEvent) {
-                this.fDialog.then((oDialog) =>oDialog.close());
-            },
-          
-
-            /***************************************************************** */
+           
             onExit: function () {
 
             },
@@ -88,7 +22,6 @@ sap.ui.define([
             onBeforeRendering: function () {
                 //el that para poder hacer la consulta de manera global
                 that.oModel = this.getView().getModel("academiaJSONModel");
-                this._setModel();
             },
 
             onSelectionItem: function(oEvent) {
@@ -100,32 +33,96 @@ sap.ui.define([
             
             
 
-            _setModel: function () {
-                let oHardcordData = {
-                    "Mail": "ignaciocoriadb@gmail.com",
-                    "ZipCode": "1142",
-                    "Country": "Argentina",
-                    "Street": "Alemanes del volga",
-                    "Number": "444"
-                };
-
-                //seteo los valores al modelo, si pongo setProperty lo guardo en / si pongo set model lo guarda directo. 
-                //El set property permite guardar varios como un arreglo de objetos
-                that.oModel.setProperty("/formulario",oHardcordData);
-                //setear el modelo a la vista 
-                that.getView().setModel(that.oModel, "AcademiaModel");
-            },
           
 
            
 
             onAfterRendering: function () {
-
+                
             },
 
-            // onPressButton: function (oEvent) {
-            //     debugger
-            // }
+          /**********NAVBAR*********************** */
+          onPressGoToMaster: function(oEvent){
+            var oListItem = oEvent.getSource();
+            var sListItemId = oListItem.getId();
+         
+            // Obtener solo el ID del elemento sin el prefijo
+            var aIdParts = sListItemId.split("--");
+            var sItemId = aIdParts[aIdParts.length - 1];
+
+
+            switch(sItemId){
+                case "_IdClubes":
+                    this.getSplitContObj().toMaster(this.createId("listClubes"));
+                    break;
+                case "_IdPlayers":
+                    this.getSplitContObj().toMaster(this.createId("listPlayers"));
+                    break
+
+
+            }
+
+
+           
+           
+        },
+        getSplitContObj: function () {
+			var result = this.byId("SplitContDemo");
+			if (!result) {
+				Log.error("SplitApp object can't be found");
+			}
+			return result;
+		},
+
+        onPressMasterBack: function () {
+			this.getSplitContObj().backMaster();
+		},
+        onListItemPress: function (oEvent) {
+			var sToPageId = oEvent.getParameter("listItem").getCustomData()[0].getValue();
+
+			this.getSplitContObj().toDetail(this.createId(sToPageId));
+			// debugger
+		},
+        /********************************** */
+
+
+        //formatear fecha
+        getFoundationDate: function(date) {
+            
+            if (!date) {
+                return "";
+            }
+            // Convertir la fecha a un objeto Date si no lo es
+            if (!(date instanceof Date)) {
+                date = new Date(date);
+               alert(date)
+            }
+
+            // Obtener el día, mes y año de la fecha
+            const dia = date.getDate();
+            const mes = date.getMonth() + 1; // Los meses en JavaScript van de 0 a 11
+            const anio = date.getFullYear();
+
+            // Formatear la fecha como "dd/mm/aaaa"
+            return `${dia}/${mes}/${anio}`;
+        },
+    
+
+        /*************CRUD***********************/
+        editClub: function(){
+            alert("Funcion editar club")
+        },
+        deleteClub: function(){
+            alert("Funcion eliminar club")
+        }
+
+
+
+
+        /************************************ */
 
         });
+
+
+        
     });
